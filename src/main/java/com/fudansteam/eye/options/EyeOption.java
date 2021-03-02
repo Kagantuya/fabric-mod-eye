ba7c45e9a0c52ae1f0ef9f5f9deccfccc5bc060e
箱子 <68680808+Kagantuya@users.noreply.github.com>
2021-03-02 14:29:09 +0800

    增加上下方位hud辅助显示，新增GUI的ToolTip，修复标记实体在检测范围内消失后检测失灵情况

--- src/main/java/com/fudansteam/eye/options/EyeOption.java
+++ src/main/java/com/fudansteam/eye/options/EyeOption.java
@@ -1,6 +1,8 @@
 package com.fudansteam.eye.options;
 
 import com.fudansteam.eye.config.EyeConfig;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.options.CyclingOption;
 import net.minecraft.client.options.DoubleOption;
 import net.minecraft.text.Text;
@@ -14,6 +16,7 @@ import net.minecraft.text.TranslatableText;
  */
 public class EyeOption {
     
+    private static final TextRenderer TEXT_RENDERER = MinecraftClient.getInstance().textRenderer;
     private static final Text SUPER_EYE_OPEN = new TranslatableText("eye.options.super_eye.open");
     private static final Text SUPER_EYE_CLOSE = new TranslatableText("eye.options.super_eye.off");
     
@@ -21,10 +24,16 @@ public class EyeOption {
             1.0D, 50.0D, 1.0F,
             gameOptions -> (double) EyeConfig.distance,
             (gameOptions, distance) -> EyeConfig.distance = distance.intValue(),
-            (gameOptions, option) -> new TranslatableText("eye.options.distance_s", EyeConfig.distance));
+            (gameOptions, option) -> {
+                option.setTooltip(TEXT_RENDERER.wrapLines(new TranslatableText("eye.options.tooltip.distance"), 200));
+                return new TranslatableText("eye.options.distance_s", EyeConfig.distance);
+            });
     
     public static final CyclingOption SUPER_EYE = new CyclingOption("eye.options.super_eye",
             (gameOptions, integer) -> EyeConfig.superEye = !EyeConfig.superEye,
-            (gameOptions, cyclingOption) -> EyeConfig.superEye ? SUPER_EYE_OPEN : SUPER_EYE_CLOSE);
+            (gameOptions, cyclingOption) -> {
+                cyclingOption.setTooltip(TEXT_RENDERER.wrapLines(new TranslatableText("eye.options.tooltip.super_eye"), 200));
+                return EyeConfig.superEye ? SUPER_EYE_OPEN : SUPER_EYE_CLOSE;
+            });
     
 }
